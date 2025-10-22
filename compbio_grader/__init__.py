58826043b33cf7b3cf0a1ad54f58c505d5f460e1
Alexander Golovshchinskiy <ag4723@ic.ac.uk>
2025-10-22 17:37:31 +0100

    Fixed __init__

--- compbio_grader/__init__.py
+++ compbio_grader/__init__.py
@@ -1,2 +1,2 @@
-from .checks import check_patterncount, check_frequencytable, check_maxmap, check_frequentwords, check_reversecomplement, check_patternmatching, check_genome_scan, check_ecoli_clumps_count
+from .checks import check_patterncount, check_frequencytable, check_maxmap, check_frequentwords, check_reversecomplement, check_patternmatching, check_genome_scan, check_ecoli_clumps_count, check_skew
 
